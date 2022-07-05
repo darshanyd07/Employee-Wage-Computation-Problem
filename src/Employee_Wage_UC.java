20938412c4bdcf7394f3ad287d6acd8a2c6368bb
champ09D <darshanmhatre709@gmail.com>
2022-07-05 12:20:59 +0530

    Welcome File Done

---
+++ src/Employee_Wage_UC.java
@@ -0,0 +1,8 @@
+public class Employee_Wage_UC
+    {
+        public static void main(String[] darsh)
+        {
+            System.out.println("Welcome to Employee Wage Computation Program");
+        }
+    }
+
