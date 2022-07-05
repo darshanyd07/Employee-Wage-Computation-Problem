4c93793cf82075c6e7fdd58851d22cc7ac08b137
champ09D <darshanmhatre709@gmail.com>
2022-07-05 12:24:48 +0530

    Employee UC1 File Done

--- src/Employee_Wage_UC.java
+++
@@ -1,8 +0,0 @@
-public class Employee_Wage_UC
-    {
-        public static void main(String[] darsh)
-        {
-            System.out.println("Welcome to Employee Wage Computation Program");
-        }
-    }
-
